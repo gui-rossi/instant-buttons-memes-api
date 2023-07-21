fe924af62a1a1c0824d923c58bf5b2e906e4487f
guirossi <guizo.rossi@gmail.com>
2023-07-20 22:24:52 -0300

    added first relationship

---
+++ Repositories/Repositories/DatabaseContext.cs
@@ -0,0 +1,27 @@
+﻿using Domains.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories.Repositories
+{
+    public class DatabaseContext : DbContext
+    {
+        public DbSet<Category> Category { get; set; }
+        public DbSet<Button> Button { get; set; }
+
+
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Button>()
+                .HasOne(s => s.Category)
+                .WithMany(g => g.Buttons)
+                .HasForeignKey(s => s.CategoryId);
+        }
+    }
+}
